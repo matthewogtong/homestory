233b70aa6541560f7a7c1232c04b04564da1a1e4
matthewogtong <ogtongm@gmail.com>
2021-01-04 17:14:52 -0500

    create title screen

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 // DOM ELEMENTS
 
 const roomNav = document.querySelector('#rooms-nav')
+const body = document.querySelector('body')
 
 // EVENT HANDLERS
 
@@ -21,13 +22,44 @@ const getRooms = () => {
 
 // RENDER FUNCTIONS
 
+//*** RENDER TITLESCREEN
+const renderAppTitle = () => {
+    const h1 = document.createElement("h1")
+    h1.textContent = "HomeStory"
+    h1.classList.add('app-title')
+    body.append(h1)
+}
+
+//*** RENDER ROOMS
 const renderRoom = (roomObj) => {
     const roomLi = document.createElement('li')
+    roomLi.dataset.id = roomObj.id
     roomLi.textContent = roomObj.name 
     roomNav.append(roomLi)
 
+    roomLi.addEventListener('click', event => {
+        renderRoomPage()
+    })
+
+}
+
+/*** RENDER ROOM PAGE */
+const renderRoomPage = () => {
+    console.log('success')
 }
 
 // INITIAL RENDER
-getRooms()
+renderAppTitle()
+
+
+// RENDERED DOM ELEMENTS
+
+/*** APP TITLE DOM ELEMENT */
+const appTitle = document.querySelector('.app-title')
+
+/*** APP TITLE EVENT LISTENER */
+appTitle.addEventListener('click', event => {
+    getRooms()
+    appTitle.remove()
+})
 
