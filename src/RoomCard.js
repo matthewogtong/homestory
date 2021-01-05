9aaa0015e86ea830c4527b2aacdbe708685b6fd2
matthewogtong <ogtongm@gmail.com>
2021-01-05 15:44:18 -0500

    add navbar and roomName h1

--- src/RoomCard.js
+++ src/RoomCard.js
@@ -4,7 +4,39 @@ class RoomCard {
     }
 
     renderRoomPage = () => {
+        // REMOVE HOME PAGE NAV
+        this.nav.remove()
         console.log(this.roomData.name)
+
+        // RENDER ROOM NAME H1
+        const roomName = document.createElement("h1")
+        roomName.textContent = this.roomData.name
+
+        // RENDER ROOM PAGE NAV
+        const roomPageNav = document.createElement("nav")
+        roomPageNav.setAttribute("id", "room-page-nav")
+
+        const homeLi = document.createElement('li')
+        homeLi.textContent = 'Home'
+
+
+        // ADD EVENT LISTENER FOR HOME
+        homeLi.addEventListener('click', () => {
+            const nav = document.createElement('nav')
+            roomName.remove()
+            roomPageNav.remove()
+            nav.setAttribute("id", "rooms-nav")
+            body.append(nav)
+            getRooms()
+        })
+
+        console.log(this.roomData)
+
+
+        // APPEND TO BODY
+        body.append(roomName)
+        roomPageNav.append(homeLi)
+        body.append(roomPageNav)
     }
 
     
@@ -14,9 +46,9 @@ class RoomCard {
         li.dataset.id = this.roomData.id
         li.textContent = this.roomData.name
 
-        const nav = document.querySelector("#rooms-nav")
-        nav.append(li)
-        body.append(nav)
+        this.nav = document.querySelector("#rooms-nav")
+        this.nav.append(li)
+        body.append(this.nav)
 
         li.addEventListener("click", this.renderRoomPage)
     }
