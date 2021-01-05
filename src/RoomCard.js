27cd78800bf9e19b0e8e56541257c3c52fb43490
matthewogtong <ogtongm@gmail.com>
2021-01-05 15:24:57 -0500

    fix rooms-navbar issue

--- src/RoomCard.js
+++ src/RoomCard.js
@@ -7,14 +7,14 @@ class RoomCard {
         console.log(this.roomData.name)
     }
 
-    render = () => {
-        const nav = document.createElement("nav")
-        nav.setAttribute("id", "rooms-nav")
+    
 
+    render = () => {
         const li = document.createElement("li")
         li.dataset.id = this.roomData.id
         li.textContent = this.roomData.name
 
+        const nav = document.querySelector("#rooms-nav")
         nav.append(li)
         body.append(nav)
 
